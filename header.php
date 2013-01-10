c2c0c3eaab6d102ed5a68aedf7c876e3a6ef3930
Adam Kiryk <akiryk@npr.org>
2013-01-10 05:47:26 -0500

    Added in adaptive images files

--- header.php
+++ header.php
@@ -13,6 +13,10 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
+
+<!-- Required for working with adaptive images (http://adaptive-images.com/): -->
+<script>document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/';</script>
+
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>

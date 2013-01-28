5bbe7df086cb589bb76fb700934da30a63a4b3af
Adam Kiryk <akiryk@npr.org>
2013-01-28 06:26:12 -0500

    Adding template files for portfolio

---
+++ blog.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template Name: Blog
+*/
+
+get_header(); ?>
+
+		<div id="primary" class="content-area">
+			<div id="content" class="site-content" role="main">
+
+			<?php // set the wp query to look for posts rather than default page content ?>
+			<?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php stratus_content_nav( 'nav-above' ); ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
+
+				<?php endwhile; ?>
+
+				<?php stratus_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'no-results', 'index' ); ?>
+
+			<?php endif; wp_reset_query(); ?> 
+
+			</div><!-- #content .site-content -->
+		</div><!-- #primary .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ content-portfolio-agg.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @package stratus
+ * @since stratus 1.0
+ * Aggregation of portfolio items -- home page.
+ * This page defines how each post or custom post type appears on an aggregation page like index, home, or archive pages.
+ * To customize how one of those pages displays this content, add the following to the appropriate page (e.g. archive.php):
+ * > get_template_part( 'content-__', get_post_format() ); // where 'content-__' says content-your-name-here
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if (has_post_thumbnail()){ ?>
+			<div class="primary-image">
+				<a class="image-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+					<?php the_post_thumbnail('thumb'); ?>
+				</a>
+			</div>
+	<?php } ?>
+	<header class="entry-header">
+		<span class="kicker"><?php echo esc_html( get_post_meta( get_the_ID(), 'micro_teaser', true ) ); ?></span>
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'stratus' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<div class="entry-meta">
+			<span class="">Created on </span><?php echo esc_html( get_post_meta( get_the_ID(), 'date_project_completed', true ) ); ?></div>
+		<?php if ( 'post' == get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php the_time( get_option( 'date_format' ) ); ?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+
+		<?php the_excerpt(); ?>
+
+		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'stratus' ), 'after' => '</div>' ) ); ?>
+	
+	</div><!-- .entry-content -->
+
+	<footer class="entry-meta">
+		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$categories_list = get_the_category_list( __( ', ', 'stratus' ) );
+				if ( $categories_list && stratus_categorized_blog() ) :
+			?>
+			<span class="cat-links">
+				<?php printf( __( 'Posted in %1$s', 'stratus' ), $categories_list ); ?>
+			</span>
+			<?php endif; // End if categories ?>
+
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$tags_list = get_the_tag_list( '', __( ', ', 'stratus' ) );
+				if ( $tags_list ) :
+			?>
+			<span class="sep"> | </span>
+			<span class="tags-links">
+				<?php printf( __( 'Tagged %1$s', 'stratus' ), $tags_list ); ?>
+			</span>
+			<?php endif; // End if $tags_list ?>
+		<?php endif; // End if 'post' == get_post_type() ?>
+
+		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+		<span class="sep"> | </span>
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'stratus' ), __( '1 Comment', 'stratus' ), __( '% Comments', 'stratus' ) ); ?></span>
+		<?php endif; ?>
+
+		<?php edit_post_link( __( 'Edit', 'stratus' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->

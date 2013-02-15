ae8e63ad5d865e993d0700016bda56ad776b77fc
Adam Kiryk <akiryk@npr.org>
2013-02-15 13:10:29 -0500

    fixing still

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function (window, document, $) {
+
+	/**
+	 * OFF CANVAS 
+	 * Manage when to show and hide the side navigation.
+	 * Handle window resize events
+	 */
+
+  var $selector = $('#sidebarButton'),
+    	$body = $( 'body' ),
+    	mobileWidth = 600,
+	    timeout = false;
+
+  $('#sidebarButton').on('click', function (e) {
+    e.preventDefault();
+    $('body').toggleClass('active');
+  });
+
+	// Remove open side menu when window is resized bigger
+	$( window ).resize( function() {
+		var browserWidth = $( window ).width();
+
+		if ( timeout !== false  )
+			clearTimeout( timeout );
+
+		timeout = setTimeout( function() {
+			if ( browserWidth > mobileWidth ) {
+				$body.removeClass('active');
+			} 
+		}, 200 );
+	} );
+
+}(this, document, jQuery));
